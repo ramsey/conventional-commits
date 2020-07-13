a7b7470d13bf0041531c5209a2673591e173f73a
Ben Ramsey <ben@benramsey.com>
2020-07-13 17:38:52 -0500

    feat(parser): implement Conventional Commits parser

---
+++ src/Conventional/Parser.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * This file is part of ramsey/captainhook-conventional
+ *
+ * ramsey/captainhook-conventional is open source software: you can distribute
+ * it and/or modify it under the terms of the MIT License (the "License"). You
+ * may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * @copyright Copyright (c) Ben Ramsey <ben@benramsey.com>
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ramsey\Conventional;
+
+use Ramsey\Conventional\Commit\Body;
+use Ramsey\Conventional\Commit\Description;
+use Ramsey\Conventional\Commit\Footer;
+use Ramsey\Conventional\Commit\Scope;
+use Ramsey\Conventional\Commit\Type;
+use Ramsey\Conventional\Exception\InvalidCommitMessage;
+
+use function count;
+use function preg_match;
+use function preg_match_all;
+use function trim;
+
+/**
+ * Validates and parses a Conventional Commits message
+ */
+class Parser
+{
+    private const COMMIT_PATTERN = "/^(?(DEFINE)(?'noun'[A-Z0-9][\w-]+)"
+        . "(?'tokenPrefix'(?:BREAKING CHANGE|(?P>noun))\ *(?:\:\ |\ \#(?=\w))))"
+        . "(?'type'(?P>noun))"
+        . "(?:\((?'scope'(?P>noun))\))?(?'bc'!)?: "
+        . "(?'desc'[[:print:]]+)"
+        . "(?:(?:\\n|\\r|\\r\\n){2}"
+        . "(?'body'.*?(?=(?P>tokenPrefix)|\$))?"
+        . "(?:(?=(?P>tokenPrefix))(?'footer'.*))?)?\$/ius";
+
+    private const FOOTER_PATTERN = "/^(?(DEFINE)(?'noun'[A-Z0-9][\w-]+)"
+        . "(?'tokenName'BREAKING CHANGE|(?P>noun))(?'tokenSeparator'\:\ |\ \#(?=\w))"
+        . "(?'tokenPrefix'(?P>tokenName)\ *(?P>tokenSeparator)))"
+        . "(?'footer'(?'token'(?P>tokenName))\ *(?'separator'(?P>tokenSeparator))\ *"
+        . "(?'value'(?:.*?)(?=(?P>tokenPrefix))|(?:.*)))/iusm";
+
+    /**
+     * Parses a commit message, returning a Commit instance or throwing an
+     * exception on failure
+     */
+    public function parse(string $commitMessage): Commit
+    {
+        if (!preg_match(self::COMMIT_PATTERN, $commitMessage, $matches)) {
+            throw new InvalidCommitMessage(
+                'Could not find a valid Conventional Commit message',
+            );
+        }
+
+        $type = new Type($matches['type']);
+        $description = new Description($matches['desc']);
+        $hasBreakingChanges = trim($matches['bc'] ?? '') === '!';
+
+        $commit = new Commit($type, $description, $hasBreakingChanges);
+
+        if (trim($matches['scope'] ?? '') !== '') {
+            $commit->setScope(new Scope($matches['scope']));
+        }
+
+        if (trim($matches['body'] ?? '') !== '') {
+            $commit->setBody(new Body($matches['body']));
+        }
+
+        foreach ($this->parseFooter($matches['footer'] ?? '') as $footer) {
+            $commit->addFooter($footer);
+        }
+
+        return $commit;
+    }
+
+    /**
+     * @return array<Footer>
+     */
+    private function parseFooter(string $footer): array
+    {
+        if (!preg_match_all(self::FOOTER_PATTERN, $footer, $matches)) {
+            return [];
+        }
+
+        /**
+         * Psalm needs this because there's no way to define the array structure
+         * above the preg_match_all() statement where $matches is instantiated.
+         *
+         * @var array{
+         *     token: list<string>,
+         *     separator: list<string>,
+         *     value: list<string>
+         * } $footerParams
+         */
+        $footerParams = $matches;
+
+        $footers = [];
+
+        for ($i = 0; $i < count($footerParams['token']); $i++) {
+            $footers[] = new Footer(
+                $footerParams['token'][$i],
+                $footerParams['value'][$i],
+                $footerParams['separator'][$i],
+            );
+        }
+
+        return $footers;
+    }
+}
---
+++ tests/Conventional/ParserTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ramsey\Test\Conventional;
+
+use Ramsey\Conventional\Exception\InvalidCommitMessage;
+use Ramsey\Conventional\Parser;
+use Ramsey\Test\RamseyTestCase;
+
+class ParserTest extends RamseyTestCase
+{
+    /**
+     * @return array<array{rawMessageFile: string, expectedMessageFile: string}>
+     */
+    public function provideRawAndExpectedCommitMessage(): array
+    {
+        return [
+            'a basic commit' => [
+                'rawMessageFile' => __DIR__ . '/../mocks/commit-message-00-raw.txt',
+                'expectedMessageFile' => __DIR__ . '/../mocks/commit-message-00-expected.txt',
+            ],
+            'a full commit' => [
+                'rawMessageFile' => __DIR__ . '/../mocks/commit-message-01-raw.txt',
+                'expectedMessageFile' => __DIR__ . '/../mocks/commit-message-01-expected.txt',
+            ],
+            'with body and no footers' => [
+                'rawMessageFile' => __DIR__ . '/../mocks/commit-message-02-raw.txt',
+                'expectedMessageFile' => __DIR__ . '/../mocks/commit-message-02-expected.txt',
+            ],
+            'with footers and no body' => [
+                'rawMessageFile' => __DIR__ . '/../mocks/commit-message-03-raw.txt',
+                'expectedMessageFile' => __DIR__ . '/../mocks/commit-message-03-expected.txt',
+            ],
+            'with simple body and single footer' => [
+                'rawMessageFile' => __DIR__ . '/../mocks/commit-message-04-raw.txt',
+                'expectedMessageFile' => __DIR__ . '/../mocks/commit-message-04-expected.txt',
+            ],
+            'with breaking change and no body adds bang' => [
+                'rawMessageFile' => __DIR__ . '/../mocks/commit-message-05-raw.txt',
+                'expectedMessageFile' => __DIR__ . '/../mocks/commit-message-05-expected.txt',
+            ],
+            'with breaking change and body adds bang' => [
+                'rawMessageFile' => __DIR__ . '/../mocks/commit-message-06-raw.txt',
+                'expectedMessageFile' => __DIR__ . '/../mocks/commit-message-06-expected.txt',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideRawAndExpectedCommitMessage
+     */
+    public function testParserAccuratelyParsesCommitMessages(
+        string $rawMessageFile,
+        string $expectedMessageFile
+    ): void {
+        $rawMessage = (string) file_get_contents($rawMessageFile);
+        $expectedMessage = (string) file_get_contents($expectedMessageFile);
+
+        $parser = new Parser();
+        $commit = $parser->parse($rawMessage);
+
+        $this->assertSame($expectedMessage, $commit->toString());
+    }
+
+    /**
+     * @return array<array{invalidMessageFile: string}>
+     */
+    public function provideInvalidCommitMessage(): array
+    {
+        return [
+            ['invalidMessageFile' => __DIR__ . '/../mocks/invalid-commit-message-00.txt'],
+            ['invalidMessageFile' => __DIR__ . '/../mocks/invalid-commit-message-01.txt'],
+            ['invalidMessageFile' => __DIR__ . '/../mocks/invalid-commit-message-02.txt'],
+            ['invalidMessageFile' => __DIR__ . '/../mocks/invalid-commit-message-03.txt'],
+            ['invalidMessageFile' => __DIR__ . '/../mocks/invalid-commit-message-04.txt'],
+            ['invalidMessageFile' => __DIR__ . '/../mocks/invalid-commit-message-05.txt'],
+            ['invalidMessageFile' => __DIR__ . '/../mocks/invalid-commit-message-06.txt'],
+            ['invalidMessageFile' => __DIR__ . '/../mocks/invalid-commit-message-07.txt'],
+            ['invalidMessageFile' => __DIR__ . '/../mocks/invalid-commit-message-08.txt'],
+        ];
+    }
+
+    /**
+     * @dataProvider provideInvalidCommitMessage
+     */
+    public function testInvalidCommitMessageThrowsException(string $invalidMessageFile): void
+    {
+        $invalidMessage = (string) file_get_contents($invalidMessageFile);
+        $parser = new Parser();
+
+        $this->expectException(InvalidCommitMessage::class);
+        $this->expectExceptionMessage('Could not find a valid Conventional Commit message');
+
+        $parser->parse($invalidMessage);
+    }
+}
